e6db7dc50c41ee4f0b6f9d47f7fbe7d0d30ea463
Nicolas Molina Monroy <hi@nicobytes.com>
2024-05-05 22:48:45 -0400

    Enable SSR in Cloudflare Pages

--- apps/website/server.ts
+++ apps/website/server.ts
@@ -1,56 +1,35 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
-import express from 'express';
-import { fileURLToPath } from 'node:url';
-import { dirname, join, resolve } from 'node:path';
-import bootstrap from './src/main.server';
+import { renderApplication } from "@angular/platform-server";
+import bootstrap from "./src/main.server";
 
-// The Express app is exported so that it can be used by serverless Functions.
-export function app(): express.Express {
-  const server = express();
-  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  const browserDistFolder = resolve(serverDistFolder, '../browser');
-  const indexHtml = join(serverDistFolder, 'index.server.html');
-
-  const commonEngine = new CommonEngine();
-
-  server.set('view engine', 'html');
-  server.set('views', browserDistFolder);
-
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
-  server.get('*.*', express.static(browserDistFolder, {
-    maxAge: '1y'
-  }));
-
-  // All regular routes use the Angular engine
-  server.get('*', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
-
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
-  });
-
-  return server;
+interface Env {
+	ASSETS: { fetch: typeof fetch };
 }
 
-function run(): void {
-  const port = process.env['PORT'] || 4000;
+// We attach the Cloudflare `fetch()` handler to the global scope
+// so that we can export it when we process the Angular output.
+// See tools/bundle.mjs
+async function workerFetchHandler(request: Request, env: Env) {
+	const url = new URL(request.url);
+	console.log("render SSR", url.href);
 
-  // Start up the Node server
-  const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+	// Get the root `index.html` content.
+	const indexUrl = new URL("/", url);
+	const indexResponse = await env.ASSETS.fetch(new Request(indexUrl));
+	const document = await indexResponse.text();
+
+	const content = await renderApplication(bootstrap, {
+		document,
+		url: url.pathname,
+	});
+
+	// console.log("rendered SSR", content);
+	return new Response(content, indexResponse);
 }
 
-run();
+export default {
+	fetch: (request: Request, env: Env) =>
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		(globalThis as any)["__zone_symbol__Promise"].resolve(
+			workerFetchHandler(request, env)
+		),
+};
